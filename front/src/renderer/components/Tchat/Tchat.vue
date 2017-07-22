357010954d3796b444d9811e4837dcfda527bd53
Ravaniss <senechal.max06@gmail.com>
2017-07-22 21:56:38 +0200

    Add every components to create the entire Tchat view correctly :)

---
+++ front/src/renderer/components/Tchat/Tchat.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="tchat">
+    <section id="cpnts">
+      <conversation></conversation>
+      <drop-zone></drop-zone>
+      <emoji-picker></emoji-picker>
+      <bar></bar>
+    </section>
+    <members></members>
+  </section>
+</template>
+
+<script>
+  import EmojiPicker from './components/EmojiPicker.vue'
+  import Members from './components/Members.vue'
+  import Bar from './components/Bar.vue'
+  import DropZone from './components/DropZone.vue'
+  import Conversation from './components/Conversation.vue'
+
+  export default {
+    components: {
+      EmojiPicker,
+      Members,
+      Bar,
+      DropZone,
+      Conversation
+    }
+  }
+</script>
