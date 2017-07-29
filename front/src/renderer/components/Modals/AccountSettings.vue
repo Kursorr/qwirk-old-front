50409641e2683d10cd5e9c5ce03a912713bfd8cc
Ravaniss <senechal.max06@gmail.com>
2017-07-29 14:06:56 +0200

    Add some script on these view for show and hide the account settings modal

--- front/src/renderer/components/Modals/AccountSettings.vue
+++ front/src/renderer/components/Modals/AccountSettings.vue
@@ -1,15 +1,18 @@
 <template>
-  <section class="modal">
+  <section class="modal" @click="closeModal($event)">
     <section class="set">
       <section class="menu">
         <h1 class="title">Paramètres Utilisateur</h1>
-        <button class="tab">Compte</button>
-        <button class="tab">Son et voix</button>
-        <button class="tab">Autres</button>
+        <button class="tab" :class="{'is-active': tab === 'account'}"
+                @click="setTab('account')">Compte</button>
+        <button class="tab" :class="{'is-active': tab === 'voice'}"
+                @click="setTab('voice')">Son et voix</button>
+        <button class="tab" :class="{'is-active': tab === 'other'}"
+                @click="setTab('other')">Autres</button>
       </section>
 
       <section class="content">
-        <section class="settings">
+        <section class="settings" v-if="tab === 'account'">
           <form action="/editUser/:id" method="POST">
             <label for="username">Votre pseudo :</label>
             <input type="text" name="username" id="username">
@@ -26,22 +29,62 @@
             <input id="file" type="file" accept="image/*" style="display:none">
 
             <label>Votre avatar</label>
-            <img alt="Avatar in setting" src="http://i0.wp.com/marioasselin.com/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg">
+            <img alt="Avatar in setting"
+                 src="http://i0.wp.com/marioasselin.com/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg" @click="avatarChange()">
 
-            <button type="submit">Enregistrer les informations</button>
+            <button type="submit" @click.prevent="update()">Enregistrer les informations</button>
           </form>
         </section>
-        <section class="settings voice">
+        <section class="settings voice" v-if="tab === 'voice'">
           Voice settings
         </section>
-        <section class="settings other">
+        <section class="settings other" v-if="tab === 'other'">
           Other settings
         </section>
         <section class="buttons">
-          <button class="success">Terminer</button>
-          <button class="danger">Se déconnecter</button>
+          <button class="success" @click="finish()">Terminer</button>
+          <button class="danger" @click="authenticateUser(null)">Se déconnecter</button>
         </section>
       </section>
     </section>
   </section>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tab: 'account'
+      }
+    },
+    methods: {
+      closeModal (e) {
+        if (e.target.classList.contains('modal')) {
+          this.$emit('close')
+        }
+      },
+      finish () {
+        this.$emit('close')
+      },
+      setTab (tabName) {
+        this.tab = tabName
+      },
+      avatarChange () {
+        this.$nextTick(() => {
+          const inputId = 'file'
+          document.getElementById(inputId).click()
+        })
+      },
+      update () {
+        this.updateUser(this.userForm)
+        this.userForm.actualPassword = ''
+        this.userForm.newPassword = ''
+      }
+    },
+    mounted () {
+      this.userForm.id = this.user.id
+      this.userForm.username = this.user.name
+      this.userForm.email = this.user.email
+    }
+  }
+</script>
--- front/src/renderer/components/Sidebar/Sections/Account.vue
+++ front/src/renderer/components/Sidebar/Sections/Account.vue
@@ -1,5 +1,10 @@
 <template>
   <section class="account">
+    <transition name="fade">
+      <account-settings v-if="modal.accountSettings"
+                        @close="setModal('accountSettings', false)"></account-settings>
+    </transition>
+
     <section class="avatar">
       <img src="http://i0.wp.com/marioasselin.com/wp-content/plugins/buddypress/bp-core/images/mystery-man.jpg" alt="avatar-account">
       <div class="status"></div>
@@ -13,7 +18,29 @@
     <section class="options">
       <button class="mute"></button>
       <button class="headset"></button>
-      <button class="settings"></button>
+      <button class="settings" @click="setModal('accountSettings', true)"></button>
     </section>
   </section>
 </template>
+
+<script>
+  import AccountSettings from '../../Modals/AccountSettings'
+
+  export default {
+    components: {
+      AccountSettings
+    },
+    data () {
+      return {
+        modal: {
+          accountSettings: false
+        }
+      }
+    },
+    methods: {
+      setModal (modalName, value) {
+        this.modal[modalName] = value
+      }
+    }
+  }
+</script>
