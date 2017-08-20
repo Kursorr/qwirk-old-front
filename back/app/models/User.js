ca99cbd789f4f6fc46bd478cb518c7a9ceed317a
Ravaniss <senechal.max06@gmail.com>
2017-08-20 20:40:59 +0200

    User model

---
+++ back/app/models/User.js
@@ -0,0 +1,26 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const Model_1 = require("../../src/Model");
+var REASON;
+(function (REASON) {
+    REASON[REASON["NOT_FOUND"] = 0] = "NOT_FOUND";
+    REASON[REASON["PASSWORD_MATCH"] = 1] = "PASSWORD_MATCH";
+    REASON[REASON["ERROR"] = 2] = "ERROR";
+})(REASON || (REASON = {}));
+class User extends Model_1.Model {
+    get Data() { return this.data; }
+    constructor(db, data = null) {
+        super(db, 'users');
+        this.data = data;
+    }
+    find(id) {
+        return new Promise((resolve, reject) => {
+            this.db.r.table(this.name).get(id).run(this.db.conn).then((result) => {
+                resolve(result);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+}
+exports.User = User;
---
+++ back/app/models/User.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+import { IDB, Model } from '../../src/Model'
+
+enum REASON
+{
+  NOT_FOUND,
+  PASSWORD_MATCH,
+  ERROR
+}
+
+interface IUserModel
+{
+  id              : string,
+  name            : string,
+  email           : string,
+  password        : string,
+  createdAt?      : Date,
+  modifiedAt?     : Date,
+  emailVerified?  : boolean
+}
+
+class User extends Model
+{
+  private data : IUserModel
+  get Data () : IUserModel { return this.data }
+
+  constructor (db : IDB, data : IUserModel = null)
+  {
+    super(db, 'users')
+    this.data = data
+  }
+
+  find (id : string) : Promise<any>
+  {
+    return new Promise<any>((resolve, reject) => {
+      this.db.r.table(this.name).get(id).run(this.db.conn).then((result) => {
+        resolve(result)
+      }).catch((err) => {
+        reject(err)
+      })
+    })
+  }
+}
+
+export { User, IUserModel }
