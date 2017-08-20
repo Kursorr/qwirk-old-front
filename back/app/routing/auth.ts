95e729686ab7639b92c6d29c1b8895af14ea00f7
Ravaniss <senechal.max06@gmail.com>
2017-08-20 20:41:34 +0200

    Some test with routes

---
+++ back/app/routing/auth.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+import * as express from 'express'
+// import * as jwt from 'express-jwt'
+// import * as token from 'jsonwebtoken'
+
+import { User } from "../models/User"
+
+const auth = express.Router()
+
+auth.get('/', async (req, res) => {
+  const user = new User(req.db)
+
+  user.find('9a5bb3a5-e493-444f-8b6c-9745a55281b5').then((userFind) => {
+    console.log(userFind)
+    res.status(200).json({ message: userFind })
+  }).catch((err) => {
+    res.status(500).json({ message: 'PAS OK' })
+  })
+})
+
+export { auth }
