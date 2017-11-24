aca5c06041901bb12b58caba3db87dd863bfc436
Ravaniss <senechal.max06@gmail.com>
2017-11-24 13:09:32 +0100

    remove unecessary file account.ts and auth.ts should be used~

--- back/app/routing/auth.ts
+++ back/app/routing/auth.ts
@@ -9,14 +9,14 @@ import { User } from "../models/User"
 const auth = express.Router()
 
 auth.get('/', async (req, res) => {
-  const user = new User(req.db)
+    const user = new User(req.db)
 
-  user.find('9a5bb3a5-e493-444f-8b6c-9745a55281b5').then((userFind) => {
-    console.log(userFind)
-    res.status(200).json({ message: userFind })
-  }).catch((err) => {
-    res.status(500).json({ message: 'PAS OK' })
-  })
+    user.find('9a5bb3a5-e493-444f-8b6c-9745a55281b5').then((userFind) => {
+        console.log(userFind)
+        res.status(200).json({ message: userFind })
+    }).catch((err) => {
+        res.status(500).json({ message: 'PAS OK' })
+    })
 })
 
 export { auth }
