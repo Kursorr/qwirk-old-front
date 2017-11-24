04c3325633f0c05bf355a21944cf47b082b4c209
Ravaniss <senechal.max06@gmail.com>
2017-11-24 13:17:22 +0100

    add socket.io to window and socket emit connection, add auth for landing screen

--- front/src/renderer/main.js
+++ front/src/renderer/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
+import axios from 'axios'
+import io from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
+
 import App from './App.vue'
 import router from './router.js'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+
+axios.defaults.baseURL = 'http://localhost:1337'
+Vue.http = Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
+
+const socketInstance = io('http://localhost:4000', {
+  transports: ['websocket']
+})
+
+Vue.use(VueSocketIO, socketInstance)
 
 /* eslint-disable no-new */
 new Vue({
