f7d345d1565a9795f71df86897c3db662344b361
Ravaniss <senechal.max06@gmail.com>
2017-12-16 00:32:56 +0100

    User model

--- back/app/models/User.js
+++ back/app/models/User.js
@@ -1,6 +1,15 @@
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Model_1 = require("./Model");
+const Hash_1 = require("../../scripts/class/Hash");
 var REASON;
 (function (REASON) {
     REASON[REASON["NOT_FOUND"] = 0] = "NOT_FOUND";
@@ -8,18 +17,44 @@ var REASON;
     REASON[REASON["ERROR"] = 2] = "ERROR";
 })(REASON || (REASON = {}));
 class User extends Model_1.Model {
-    get Data() { return this.data; }
+    get Data() {
+        return this.data;
+    }
     constructor(db, data = null) {
         super(db, 'users');
         this.data = data;
     }
-    find(id) {
-        return new Promise((resolve, reject) => {
-            this.db.r.table(this.table).get(id).run(this.db.conn).then((result) => {
-                resolve(result);
-            }).catch((err) => {
-                reject(err);
-            });
+    insert(data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.db.r.table(this.table).insert(data).run(this.db.conn);
+        });
+    }
+    filter(data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.db.r.table(this.table).filter(data).run(this.db.conn);
+        });
+    }
+    opt(id, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.db.r.table(this.table).get(id).update(data).run(this.db.conn);
+        });
+    }
+    update(id, data, password) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const cursor = yield this.filter({ id });
+            const { password: result } = yield cursor.next();
+            if (password) {
+                const verifPassword = yield Hash_1.Password.compare(password, result);
+                if (verifPassword) {
+                    return yield this.opt(id, data);
+                }
+                else {
+                    throw new Error('Mot de passe invalide');
+                }
+            }
+            else {
+                return yield this.opt(id, data);
+            }
         });
     }
 }
--- back/app/models/User.ts
+++ back/app/models/User.ts
@@ -1,47 +1,73 @@
-'use strict';
+'use strict'
 
 import { IDB, Model } from './Model'
+import { Password } from '../../scripts/class/Hash'
 
 enum REASON
 {
-  NOT_FOUND,
-  PASSWORD_MATCH,
-  ERROR
+	NOT_FOUND,
+	PASSWORD_MATCH,
+	ERROR
 }
 
 interface IUserModel
 {
-  id              : string,
-  tag             : number,
-  name            : string,
-  email           : string,
-  password        : string,
-  createdAt?      : Date,
-  modifiedAt?     : Date,
-  emailVerified?  : boolean
+	id              : string,
+	tag             : number,
+	pseudo          : string,
+	email           : string,
+	password        : string,
+	createdAt?      : Date,
+	modifiedAt?     : Date,
+	emailVerified?  : boolean,
+	file?           : string
 }
 
-class User extends Model
-{
-  private data: IUserModel
-  get Data (): IUserModel { return this.data }
+class User extends Model {
+    private data: IUserModel
 
-  constructor (db: IDB, data: IUserModel = null)
-  {
-    super(db, 'users')
-    this.data = data
-  }
+    get Data(): IUserModel {
+    	return this.data
+    }
 
-  find (id: string): Promise<any>
-  {
-    return new Promise<any>((resolve, reject) => {
-      this.db.r.table(this.table).get(id).run(this.db.conn).then((result) => {
-        resolve(result)
-      }).catch((err) => {
-        reject(err)
-      })
-    })
-  }
+    constructor(db: IDB, data: IUserModel = null)
+    {
+        super(db, 'users')
+        this.data = data
+    }
+
+	async insert (data: object): Promise<any>
+	{
+		return this.db.r.table(this.table).insert(data).run(this.db.conn)
+	}
+
+    async filter (data: object): Promise<any>
+    {
+    	return this.db.r.table(this.table).filter(data).run(this.db.conn)
+    }
+
+  	private async opt (id: string, data: object): Promise<any>
+  	{
+  		return this.db.r.table(this.table).get(id).update(data).run(this.db.conn)
+  	}
+
+    async update (id: string, data: object, password?: string): Promise<any>
+    {
+    	const cursor = await this.filter({ id })
+	    const { password: result } = await cursor.next()
+
+        if (password) {
+	        const verifPassword = await Password.compare(password, result)
+
+	        if (verifPassword) {
+		        return await this.opt(id, data)
+	        } else {
+		        throw new Error('Mot de passe invalide')
+	        }
+        } else {
+	        return await this.opt(id, data)
+        }
+    }
 }
 
 export { User, IUserModel }
