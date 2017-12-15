ef75796ccc9717a33b5a0f1a5abb5101e74d92b0
Ravaniss <senechal.max06@gmail.com>
2017-12-16 00:30:45 +0100

    Add decode image and get path image functionnalities

--- back/scripts/Helper.js
+++ back/scripts/Helper.js
@@ -4,7 +4,36 @@ const isEmpty = (field) => {
     return (field === undefined || field === null || field === '' || field === [] || field === false);
 };
 exports.isEmpty = isEmpty;
-const randomTag = (min, max) => {
-    return '#' + ((Math.random() * (max - min + 1) | 0) + min);
+const randomTag = (min = 1000, max = 9999) => {
+    return (Math.random() * (max - min + 1) | 0) + min;
 };
 exports.randomTag = randomTag;
+const decodeBase64Image = (data) => {
+    if (!data)
+        return '';
+    const matches = data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    let response = {};
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+    return response;
+};
+exports.decodeBase64Image = decodeBase64Image;
+const imgPath = (result) => {
+    if (!result)
+        return '/home/ravaniss/Development/Qwirk/back/avatars/anonym.jpg';
+    const imgPath = '/home/ravaniss/Development/Qwirk/back/avatars/';
+    const imgTypeRegularExpression = /\/(.*?)$/;
+    const imgType = result.type.match(imgTypeRegularExpression);
+    const imgName = randomText(14);
+    const uploadedImgPath = imgPath + imgName + '.' + imgType[1];
+    return uploadedImgPath;
+};
+exports.imgPath = imgPath;
+function randomText(value) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567';
+    for (let i = 0; i < value; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+exports.randomText = randomText;
--- back/scripts/Helper.ts
+++ back/scripts/Helper.ts
@@ -2,8 +2,42 @@ const isEmpty = (field) => {
   return (field === undefined || field === null || field === '' || field === [] || field === false)
 }
 
-const randomTag = (min: number, max: number) => {
-    return '#' + ((Math.random() * (max - min + 1) | 0) + min)
+const randomTag = (min: number = 1000, max: number = 9999) => {
+    return (Math.random() * (max - min + 1) | 0) + min
 }
 
-export { isEmpty, randomTag }
+const decodeBase64Image = (data: string) => {
+	if (!data) return ''
+
+	const matches = data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+	let response: any = {}
+
+	response.type = matches[1]
+	response.data = new Buffer(matches[2], 'base64')
+
+	return response
+}
+
+const imgPath = (result: any) => {
+	if (!result) return '/home/ravaniss/Development/Qwirk/back/avatars/anonym.jpg'
+
+	const imgPath = '/home/ravaniss/Development/Qwirk/back/avatars/'
+	const imgTypeRegularExpression = /\/(.*?)$/
+	const imgType = result.type.match(imgTypeRegularExpression)
+	const imgName = randomText(14)
+	const uploadedImgPath = imgPath + imgName + '.' + imgType[1]
+
+	return uploadedImgPath
+}
+
+function randomText(value) {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567'
+
+	for (let i = 0; i < value; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length))
+
+	return text
+}
+
+export { isEmpty, randomTag, decodeBase64Image, imgPath, randomText }
