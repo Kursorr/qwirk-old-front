12bac27d0159086ad641d1dbe300ffbed87ffd00
Ravaniss <senechal.max06@gmail.com>
2018-02-15 21:35:19 +0100

    Splash screen functionnality (commented for now)

--- front/src/main/index.js
+++ front/src/main/index.js
@@ -67,3 +67,60 @@ app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
  */
+/*
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+app.on('ready', () => {
+  let mainWindow = null
+  let loading = new BrowserWindow({
+    show: false,
+    frame: false,
+    height: 600,
+    width: 400
+  })
+
+  loading.once('show', () => {
+    mainWindow = new BrowserWindow({
+      height: 900,
+      useContentSize: true,
+      width: 1420,
+      backgroundColor: '#36393e',
+      frame: false,
+      show: false
+    })
+    mainWindow.webContents.once('dom-ready', () => {
+      mainWindow.show()
+      loading.hide()
+      loading.close()
+    })
+    // long loading html
+    setTimeout(() => {
+      mainWindow.loadURL(winURL)
+    }, 8000)
+  })
+  loading.loadURL('https://stackoverflow.com/questions/42292608/electron-loading-animation')
+  loading.show()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    console.log('ok')
+  }
+}) */
