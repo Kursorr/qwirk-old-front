51142133483d0f6783cb967c2302daa5d2cecf42
Ravaniss <senechal.max06@gmail.com>
2018-02-18 18:57:06 +0100

    node on package.json ;)

--- front/src/renderer/components/Modals/AccountSettings.vue
+++ front/src/renderer/components/Modals/AccountSettings.vue
@@ -79,112 +79,7 @@
 
         <!-- Security View -->
         <section class="settings" id="security" v-if="tab === 'confidSecurity'">
-          <h2 class="titleSection">Confidentialité & Sécurité</h2>
-          <section class="somethinghere" id="privateMsgDanger">
-            <h2 class="information">Message privé sans danger</h2>
-            <p class="section-description">Analyser et supprimer automatiquement les messages privés reçus renfermant un contenu explicite.</p>
-            <div>
-              <input id="moyen" name="security" type="radio">
-              <section class="checkbox green">
-                <div>
-                  <label class="fake-checkbox" for="moyen"></label>
-                </div>
-                <div>
-                  <h3 class="sec-title medium">
-                    Protégez-moi
-                  </h3>
-                  <p class="section-description">
-                    Analyser les messages privés de tout le monde.
-                  </p>
-                </div>
-              </section>
-            </div>
-            <div>
-              <input id="fort" name="security" type="radio">
-              <section class="checkbox orange">
-                <div>
-                  <label class="fake-checkbox" for="fort"></label>
-                </div>
-                <div>
-                  <h3 class="sec-title hard">
-                    Mes amis sont sympas
-                  </h3>
-                  <p class="section-description">
-                    Analyser les messages privés de tout le monde hormis les amis.
-                  </p>
-                </div>
-              </section>
-            </div>
-            <div>
-              <input id="extreme" name="security" type="radio">
-              <section class="checkbox red">
-                <div>
-                  <label class="fake-checkbox" for="extreme"></label>
-                </div>
-                <div>
-                  <h3 class="sec-title extreme">
-                    Je vis dangereusement
-                  </h3>
-                  <p class="section-description">
-                    Désactiver cette option. Ne rien analyser. Allons tout droit vers le côté obscur.
-                  </p>
-                </div>
-              </section>
-            </div>
-          </section>
-          <section class="somethinghere" id="paramsConfidentialityServer">
-            <h2 class="information">Paramètres confidentialité serveur par défaut</h2>
-            <ul>
-              <li class="param-content">
-                <section class="param">
-                  <p>Autoriser les messages privés provenant de membres de groupes/channels</p>
-                  <div class="checkbox-grey">
-                    <input type="checkbox" value="1" class="checkboxNone" id="allowPrivateMsg"
-                           name="allowPrivateMsg"><label for="allowPrivateMsg" class="labelCircle"></label>
-                    <div class="checkboxDiv"></div>
-                  </div>
-                </section>
-                <p class="section-description">
-                  Ce paramètre s'applique lorsque vous rejoignez un nouveau serveur. Il ne s'applique pas à ceux que vous avez joint auparavant.
-                </p>
-              </li>
-            </ul>
-          </section>
-          <section class="somethinghere" id="canIAddU">
-            <h2 class="information">Qui peut vous ajouter en ami</h2>
-            <ul>
-              <li class="param-content">
-                <section class="param">
-                  <p>Tout le monde</p>
-                  <div class="checkbox-grey">
-                    <input type="checkbox" value="1" class="checkboxNone" id="everyone"
-                           name="everyone"><label for="everyone" class="labelCircle"></label>
-                    <div class="checkboxDiv"></div>
-                  </div>
-                </section>
-              </li>
-              <li class="param-content">
-                <section class="param">
-                  <p>Les amis de vos amis</p>
-                  <div class="checkbox-grey">
-                    <input type="checkbox" value="1" class="checkboxNone" id="ftof"
-                           name="ftof"><label for="ftof" class="labelCircle"></label>
-                    <div class="checkboxDiv"></div>
-                  </div>
-                </section>
-              </li>
-              <li class="param-content">
-                <section class="param">
-                  <p>Membre du serveur</p>
-                  <div class="checkbox-grey">
-                    <input type="checkbox" value="1" class="checkboxNone" id="groupChanMember"
-                           name="groupChanMember"><label for="groupChanMember" class="labelCircle"></label>
-                    <div class="checkboxDiv"></div>
-                  </div>
-                </section>
-              </li>
-            </ul>
-          </section>
+          <security></security>
         </section>
 
         <!-- ConfCall View -->
@@ -547,6 +442,7 @@
             </section>
           </section>
         </section>
+
         <section class="buttons">
           <button class="success" @click="finish()">Terminer</button>
           <button class="danger" @click="disconnect()">Se déconnecter</button>
@@ -562,12 +458,14 @@
 
   import Avatar from '../Contents/components/Avatar.vue'
   import Upload from '../Contents/components/Upload.vue'
+  import Security from './Params/Security.vue'
 
   export default {
     store,
     components: {
       Avatar,
-      Upload
+      Upload,
+      Security
     },
     data () {
       return {
