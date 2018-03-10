82c411095e7e959347e4b8cd94b198a1c09f5601
Ravaniss <senechal.max06@gmail.com>
2018-03-10 15:07:00 +0100

    Implementation of TestCafé and Unit Testing with Chai

--- front/test/unit/specs/LandingPage.spec.js
+++ front/test/unit/specs/LandingPage.spec.js
@@ -1,9 +1,17 @@
-var assert = require('assert')
+// const assert = require('assert')
+import Vue from 'vue'
+import view from '../../../src/renderer/components/Modals/components/User/Security.vue'
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
-    })
+describe('view', () => {
+  // Inspecter l'objet d'options du composant
+  it('a le hook `created`', () => {
+    expect(view.created).to.be.a('function')
   })
+
+  // Évaluer les résultats des fonctions dans
+  // l'objet d'options du composant
+  it('contient les données par défaut', () => {
+    expect(view.data).to.be.a('function')
+  })
+
 })
