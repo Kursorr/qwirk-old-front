4e39edefdecb06e89fc42762855c0f6f6f9a3cc9
Ravaniss <senechal.max06@gmail.com>
2018-03-11 14:10:48 +0100

    config multilanguages on main.js

--- front/src/renderer/main.js
+++ front/src/renderer/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import axios from 'axios'
 import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
@@ -17,11 +18,31 @@ const socketInstance = io('http://localhost:3000', {
   transports: ['websocket']
 })
 
+Vue.use(VueI18n)
 Vue.use(VueSocketIO, socketInstance)
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages // set locale messages
+})
 
 /* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
+  i18n,
   template: '<App/>'
 }).$mount('#app')
