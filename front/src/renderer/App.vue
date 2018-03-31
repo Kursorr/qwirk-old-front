0782ffec089cc2bdc22e22c9234d476490cc6d95
Ravaniss <senechal.max06@gmail.com>
2018-03-31 23:59:34 +0200

    remove useless code on App.vue

--- front/src/renderer/App.vue
+++ front/src/renderer/App.vue
@@ -12,25 +12,10 @@
 </template>
 
 <script>
-  import Vuex from 'vuex'
-
-  import Sidebar from './components/Sidebar/Sidebar.vue'
-  import Auth from './components/Modals/Auth.vue'
-  import store from './vuex/store'
-
   require('../../static/style/style.css')
 
   export default {
-    store,
     name: 'front',
-    components: {
-      Sidebar, Auth
-    },
-    computed: {
-      ...Vuex.mapGetters([
-        'user'
-      ])
-    },
     mounted () {
       window.io = this.$socket
       this.$socket.emit('connection')
