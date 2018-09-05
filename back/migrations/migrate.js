709c44c486274b371f33ba9e4f685419319a1c1b
Smaxime <senechal.max06@gmail.com>
2018-09-05 08:18:11 +0200

    Create the app should create db and tables directly

--- back/migrations/migrate.js
+++ back/migrations/migrate.js
@@ -1,19 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const r = require("rethinkdb");
-const process = require('process');
 const config_1 = require("../config/config");
 const tables = ['users', 'messages'];
-r.connect(config_1.database, (err, conn) => {
-    r.dbList().contains(config_1.database.db)
+r.connect(config_1.database, (err, conn) => __awaiter(this, void 0, void 0, function* () {
+    yield r.dbList().contains(config_1.database.db)
         .do(databaseExist => {
         return r.branch(databaseExist, {
             dbs_created: 0
         }, r.dbCreate(config_1.database.db));
     }).run(conn);
-    r(tables)
+    yield r(tables)
         .difference(r.db(config_1.database.db).tableList())
         .forEach(table => r.db(config_1.database.db).tableCreate(table))
         .run(conn);
-    process.exit(0);
-});
+    yield conn.close();
+}));
--- back/migrations/migrate.ts
+++ back/migrations/migrate.ts
@@ -1,12 +1,11 @@
 import * as r from 'rethinkdb'
-const process = require('process')
 
 import { database } from '../config/config'
 
 const tables = ['users', 'messages']
 
-r.connect(database, (err, conn) => {
-  r.dbList().contains(database.db)
+r.connect(database, async (err, conn) => {
+  await r.dbList().contains(database.db)
     .do(databaseExist => {
       return r.branch(databaseExist, {
         dbs_created: 0 },
@@ -14,10 +13,10 @@ r.connect(database, (err, conn) => {
       )
     }).run(conn);
 
-  r(tables)
+  await r(tables)
     .difference(r.db(database.db).tableList())
     .forEach(table => r.db(database.db).tableCreate(table))
     .run(conn)
 
-  process.exit(0)
+  await conn.close()
 })
