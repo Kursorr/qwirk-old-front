649f47404acf2e73d9c05a3c104ca26c51d06c42
Ravaniss <senechal.max06@gmail.com>
2018-09-15 18:14:16 +0200

    activ spashscreen

--- front/src/main/index.js
+++ front/src/main/index.js
@@ -1,76 +1,6 @@
-'use strict'
-
 import { app, BrowserWindow } from 'electron'
 
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
-}
-
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
-
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 900,
-    useContentSize: true,
-    width: 1420,
-    backgroundColor: '#36393e',
-    frame: false
-  })
-
-  mainWindow.loadURL(winURL)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
-/*
-'use strict'
-
-import { app, BrowserWindow } from 'electron'
+const wait = 8000
 
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
@@ -85,7 +15,7 @@ app.on('ready', () => {
   let mainWindow = null
   let loading = new BrowserWindow({
     show: false,
-    frame: false,
+    frame: true,
     height: 600,
     width: 400
   })
@@ -107,7 +37,7 @@ app.on('ready', () => {
     // long loading html
     setTimeout(() => {
       mainWindow.loadURL(winURL)
-    }, 8000)
+    }, wait)
   })
   loading.loadURL('https://stackoverflow.com/questions/42292608/electron-loading-animation')
   loading.show()
@@ -123,4 +53,4 @@ app.on('activate', () => {
   if (mainWindow === null) {
     console.log('ok')
   }
-}) */
+})
