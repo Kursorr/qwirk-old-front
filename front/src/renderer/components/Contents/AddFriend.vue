0dedf5c231be9f1664f04d50cfc3c68e7cfa8214
Ravaniss <senechal.max06@gmail.com>
2019-08-01 21:07:21 +0200

    Front - AddFriend - Manage error if user is on db

--- front/src/renderer/components/Contents/AddFriend.vue
+++ front/src/renderer/components/Contents/AddFriend.vue
@@ -2,6 +2,12 @@
   <section id="add_friend">
     <h1>Ajouter un ami</h1>
     <p>Vous pouvez ajouter un ami grâce à leur identifiant Qwirk.</p>
+    <div v-if="request.requestResult === false">
+      Mhm, ça n'a pas marché. Vérifie bien sur la casse, l'orthographe, les espaces et les chiffres sont corrects.
+    </div>
+    <div v-if="request.requestResult === true">
+      Mission accompli ! Attends, qu'est ce que... C'est un oiseau ? c'est un avion ? Non, c'est Superman qui envoie ta demande d'amis à {{ request.pseudo }} ! Un mec bien, ce Clark.
+    </div>
     <form method="POST" @submit.prevent="addNewFriend()">
       <input type="text" placeholder="Entrez un identifiant qwirk #0000" v-model="newFriend">
       <button>Envoyer une requête d'ami</button>
@@ -20,10 +26,16 @@
       ...mapGetters([
         'user'
       ])
+    },
+    sockets: {
+      sendFriendRequest (request) {
+        this.request = request
+      }
     }
   })
   export default class AddFriend extends Vue {
     private newFriend: string = ''
+    public request = {}
 
     public addNewFriend () {
       this.$socket.emit('addFriend', {
