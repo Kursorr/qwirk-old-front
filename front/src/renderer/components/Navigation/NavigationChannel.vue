5af810e31c08576a4bbb488507e3e5a63adb54a6
Ravaniss <senechal.max06@gmail.com>
2019-08-21 13:58:59 +0200

    NavigationChannel component creation

---
+++ front/src/renderer/components/Navigation/NavigationChannel.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <section class="components">
+      channels here
+    </section>
+    <profile></profile>
+  </section>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import Profile from './Sections/Profile.vue'
+
+  @Component({
+    components: {
+      Profile
+    }
+  })
+  export default class NavigationChannel extends Vue {
+  }
+</script>
