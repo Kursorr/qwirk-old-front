60050688792aaf5a2d1aa47ab9127c6bf41087fc
Ravaniss <senechal.max06@gmail.com>
2019-08-22 01:07:45 +0200

    Server - new component that will display info about the server

---
+++ front/src/renderer/components/Navigation/Sections/Server.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      <slot></slot>
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class Server extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
