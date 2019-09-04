97dc170787f10b797d23bdd320acc3c95f262dd1
Ravaniss <senechal.max06@gmail.com>
2019-09-04 06:32:03 +0200

    Front - Create a component to switch between tchat channel

--- front/src/renderer/components/Navigation/NavigationChannel.vue
+++ front/src/renderer/components/Navigation/NavigationChannel.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="container">
     <section class="components">
-      <server>
-        {{ serverName }}
+      <server v-if="currentServer">
+        {{ currentServer.name }}
       </server>
-      <div v-for="name in channelsName">
-        {{ name.name }}
+
+      <div class="server" v-for="channel in channels">
+        <router-link @click.native="choiceChannel(channel)"
+                     :to="{name: 'tchat', params: { convId: channel.id, type: 'public' }}"
+                     class="chan set" tag="div">
+          {{ channel.name }}
+        </router-link>
       </div>
+
     </section>
     <profile></profile>
   </section>
@@ -14,23 +20,60 @@
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
+  import { Store, mapGetters, mapActions } from 'vuex'
+
   import Profile from './Sections/Profile.vue'
   import Server from './Sections/Server.vue'
+  import pusherStore from '../../store/PusherStore'
 
   @Component({
+    Store,
     components: {
       Profile,
       Server
     },
+    computed: {
+      ...mapGetters([
+        'currentServer',
+        'currentChannel'
+      ])
+    },
+    methods: {
+      ...mapActions([
+        'addMessage',
+        'upWaitMsg',
+        'setChannel'
+      ])
+    },
     sockets: {
-      updateChannel (server) {
-        this.serverName = server.name
-        this.channelsName = server.channelsName
+      updateChannel (channel) {
+        this.channelName = channel.name
+        this.channels = channel.channels
+        this.channels.map((channel) => {
+          pusherStore.subscribe(`ch-${channel.id}`, this.processChannel)
+        })
       }
     }
   })
   export default class NavigationChannel extends Vue {
-    serverName = ''
-    channelsName = ''
+    channelName = ''
+    channels = ''
+
+    public processChannel(data) {
+      console.log(this.currentChannel.id)
+      console.log(data)
+      if (this.currentChannel.id === data.channelName) {
+        this.addMessage([data.msg])
+      } else {
+        this.upWaitMsg(data.channelName)
+      }
+    }
+
+    public choiceChannel(channel) {
+      const data = channel
+      data.serverId = this.currentServer.id
+      this.setChannel(channel)
+      this.$socket.emit('GET::MESSAGES', data)
+    }
   }
 </script>
