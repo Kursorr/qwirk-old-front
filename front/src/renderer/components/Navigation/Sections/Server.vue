90ca14d2a268832c02341f3b411622e31db2e82d
Ravaniss <senechal.max06@gmail.com>
2019-09-07 00:36:35 +0200

    Server - some css

--- front/src/renderer/components/Navigation/Sections/Server.vue
+++ front/src/renderer/components/Navigation/Sections/Server.vue
@@ -11,10 +11,20 @@
 
   @Component
   export default class Server extends Vue {
-
   }
 </script>
 
 <style lang="scss" scoped>
+  div {
+    box-shadow: 0 1px 0 rgba(0,0,0,.2), 0 1.5px 0 rgba(0,0,0,.05), 0 2px 0 rgba(0,0,0,.05);
+    margin-bottom: 15px;
 
+    h1 {
+      margin-left: 18px;
+      font-size: 16px;
+      line-height: 20px;
+      font-weight: 500;
+      padding: 14px 0;
+    }
+  }
 </style>
