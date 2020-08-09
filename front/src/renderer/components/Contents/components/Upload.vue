2d54e1abc67b6da3bc7b5eb9e000d993d16277f2
Ravaniss <senechal.max06@gmail.com>
2020-08-09 20:01:40 +0200

    Update Upload component

--- front/src/renderer/components/Contents/components/Upload.vue
+++ front/src/renderer/components/Contents/components/Upload.vue
@@ -2,7 +2,7 @@
   <div class="upload">
     <section class="logo-ce">
       <section class="logo-group">
-        <avatar :url="userOrImage" size="big"></avatar>
+        <avatar :url="image" size="big"/>
         <input type="file" class="file-input" accept=".jpg,.jpeg,.png,.gif" @change="onFileChange">
       </section>
     </section>
@@ -11,59 +11,59 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import { mapGetters } from 'vuex'
+  import { Component, Vue } from 'vue-property-decorator'
+  import { mapGetters } from 'vuex'
 
-import Avatar from './Avatar.vue'
-import { ComponentOptions } from 'vue'
+  import Avatar from './Avatar.vue'
+  import { ComponentOptions } from 'vue'
 
-@Component({
-  components: {
-    Avatar
-  },
-  computed: {
-    ...mapGetters([
-      'user'
-    ])
-  }
-} as ComponentOptions<Upload>)
-export default class Upload extends Vue {
-
-  get userOrImage () {
-    if (this.image !== null) {
-      return this.image
+  @Component({
+    components: {
+      Avatar
+    },
+    computed: {
+      ...mapGetters([
+        'user'
+      ])
     }
-    return null
-  }
-  private image?: any = null
-  private user: any
+  } as ComponentOptions<Upload>)
+  export default class Upload extends Vue {
+    private image: string = ''
+    private user: any
 
-  public onFileChange (e: any) {
-    const files = e.target.files || e.dataTransfer.files
-    if (!files.length) { return }
-    return this.createImage(files[0])
-  }
-
-  public changeImage (image: string) {
-    this.image = image
-    this.$emit('change', this.image)
-  }
-
-  private createImage (file: Blob) {
-    const reader = new FileReader()
-
-    reader.onload = (e: any) => {
-      this.changeImage(e.target.result)
+    get userOrAnonymImg () {
+      if (this.image !== '') {
+        return this.image
+      }
+      return null
     }
-    reader.readAsDataURL(file)
-  }
 
-  private mounted () {
-    if (this.user) {
-      this.image = this.user.avatar
+    public onFileChange (e: any) {
+      const files = e.target.files || e.dataTransfer.files
+      if (!files.length) { return }
+      return this.createImage(files[0])
+    }
+
+    public changeImage (image: string) {
+      this.image = image
+      this.$emit('change', this.image)
+    }
+
+    private createImage (file: Blob) {
+      const reader = new FileReader()
+
+      reader.onload = (e: any) => {
+        this.changeImage(e.target.result)
+      }
+      reader.readAsDataURL(file)
+    }
+
+    private mounted () {
+      if (this.user) {
+        this.image = this.user.avatar
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
