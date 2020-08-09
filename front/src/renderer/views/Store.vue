4bc6082eef608fb5aeca7d61b7b899c1f1d4b9a9
Ravaniss <senechal.max06@gmail.com>
2020-08-10 01:44:03 +0200

    Store - Add carousel / place element - doesn't work yet and need real images

--- front/src/renderer/views/Store.vue
+++ front/src/renderer/views/Store.vue
@@ -4,27 +4,41 @@
         iconName="Boutique"
         :filters="true"
         :separator="true">
-      <library-icon/>
+      <shop-icon />
     </menu-bar>
 
     <div id="image">
       <div class="content">
-        <div class="text">
-          <span class="featured">featured game</span>
-          <h1 class="title">Dead Cells</h1>
-          <p class="infos">
-            Explore an ever changing castle in this rogue-like
-            action platformer. Each death progresses towards
-            unlocking new spells, weapons, or levels.
-          </p>
+        <div class="details">
+          <div class="text">
+            <span class="featured">featured game</span>
+            <h1 class="title">Dead Cells</h1>
+            <p class="infos">
+              Explore an ever changing castle in this rogue-like
+              action platformer. Each death progresses towards
+              unlocking new spells, weapons, or levels.
+            </p>
 
-          <div class="priceLearnMore">
-            <span class="price">$24.99</span>
-            <button>Learn More</button>
+            <div class="priceLearnMore">
+              <span class="price">$24.99</span>
+              <button>Learn More</button>
+            </div>
           </div>
+
+          <video controls class="video" src="../assets/store/dead-cells-trailer.mp4" ref="video"></video>
         </div>
 
-        <video controls class="video" src="../assets/store/dead-cells-trailer.mp4" ref="video"></video>
+        <div class="carousel">
+          <icon-base
+              icon-name="leftArrow"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24">
+            <left-arrow/>
+          </icon-base>
+
+          <div class="carousel-item"></div>
+        </div>
       </div>
     </div>
 
@@ -36,12 +50,16 @@
   import { Component, Vue } from 'vue-property-decorator'
 
   import MenuBar from '../components/Contents/MenuBar.vue'
-  import LibraryIcon from '../components/Svg/Menu/LibraryIcon.vue'
+  import ShopIcon from '../components/Svg/Menu/ShopIcon.vue'
+  import IconBase from '../components/Svg/IconBase.vue'
+  import LeftArrow from '../components/Svg/LeftArrow.vue'
 
   @Component({
     components: {
       MenuBar,
-      LibraryIcon
+      IconBase,
+      ShopIcon,
+      LeftArrow
     }
   })
   export default class Store extends Vue {}
@@ -59,60 +77,80 @@
 
     .content {
       width: 80%;
+      height: 100%;
       display: flex;
+      flex-direction: column;
 
-      .text {
-        width: 50%;
+      .details {
         display: flex;
-        flex-direction: column;
-        justify-content: center;
+        height: 90%;
+        align-items: center;
 
-        .featured {
-          text-transform: uppercase;
-          color: #D6D7D8;
-          font-size: 12px;
-        }
-
-        .title {
-          margin: 10px 0;
-          color: #FFF;
-          font-size: 24px;
-        }
-
-        .infos {
-          width: 75%;
-          color: #FFF;
-          margin-bottom: 25px;
-          line-height: 25px;
-        }
-
-        .priceLearnMore {
+        .text {
+          width: 50%;
           display: flex;
-          align-items: center;
+          flex-direction: column;
+          justify-content: center;
 
-          .price {
-            color: #FFF;
-            margin-right: 20px;
+          .featured {
+            text-transform: uppercase;
+            color: #D6D7D8;
+            font-size: 12px;
           }
 
-          button {
-            background: transparent;
-            border: solid #7e7f84 0.5px;
-            font-size: 12px;
-            padding: 8px 16px;
+          .title {
+            margin: 10px 0;
+            color: #FFF;
+            font-size: 24px;
+          }
 
-            &:hover {
-              border-color: #FFF;
+          .infos {
+            width: 75%;
+            color: #FFF;
+            margin-bottom: 25px;
+            line-height: 25px;
+          }
+
+          .priceLearnMore {
+            display: flex;
+            align-items: center;
+
+            .price {
+              color: #FFF;
+              margin-right: 20px;
+            }
+
+            button {
+              background: transparent;
+              border: solid #7e7f84 0.5px;
+              font-size: 12px;
+              padding: 8px 16px;
+
+              &:hover {
+                border-color: #FFF;
+              }
             }
           }
         }
+
+        .video {
+          width: 50%;
+          object-position: center;
+          object-fit: cover;
+        }
       }
 
-      .video {
-        height: 270px;
-        width: 50%;
-        object-position: center;
-        object-fit: cover;
+      .carousel {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .carousel-item {
+          background-color: mediumpurple;
+          height: 44px;
+          width: 80px;
+          border-radius: 4px;
+        }
       }
     }
   }
